d36ad32108a11812b08ac68f83d8636c8caf492f
ksg <ksg001227@naver.com>
2024-11-18 16:49:45 +0900

    feat: UserController 완성

--- src/main/java/com/kuit/kuit4serverauth/controller/UserController.java
+++ src/main/java/com/kuit/kuit4serverauth/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.kuit.kuit4serverauth.controller;
 
+import com.kuit.kuit4serverauth.repository.UserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +13,22 @@ public class UserController {
     @GetMapping("/profile")
     public ResponseEntity<String> getProfile(HttpServletRequest request) {
         // TODO : 로그인 한 사용자면 username 이용해 "Hello, {username}" 반환하기
+        String username = (String) request.getAttribute("username");
+
+        if (username != null) {
+            return ResponseEntity.ok("Hello, " + username);
+        }
+
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
     }
 
     @GetMapping("/admin")
     public ResponseEntity<String> getAdmin(HttpServletRequest request) {
         // TODO: role이 admin이면 "Hello, admin" 반환하기
+        String role = (String) request.getAttribute("role");
+        if (role != null && role.equals("ROLE_ADMIN")) {
+            return ResponseEntity.ok("Hello, admin");
+        }
         return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Forbidden");
     }
 }
