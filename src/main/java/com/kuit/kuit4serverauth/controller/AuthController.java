1937728032d501788dcf7ec60bfc6ed8f7084386
yuna <yjeong1217@gmail.com>
2024-11-22 22:08:45 +0900

    기본 미션

--- src/main/java/com/kuit/kuit4serverauth/controller/AuthController.java
+++ src/main/java/com/kuit/kuit4serverauth/controller/AuthController.java
@@ -25,17 +25,18 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> credentials) {
+    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> credentials) {    // todo 파라미터 dto로 리팩토링
         String username = credentials.get("username");
         String password = credentials.get("password");
 
+        // todo 서비스 레이어
         User user = userRepository.findByUsername(username);
         if (user == null || !user.getPassword().equals(password)) {
             throw new CustomException(ErrorCode.INVALID_USERNAME_OR_PASSWORD);
         }
 
         String token = jwtUtil.generateToken(user.getUsername(), user.getRole());
-        Map<String, String> response = new HashMap<>();
+        Map<String, String> response = new HashMap<>();     // todo dto
         response.put("token", token);
         return ResponseEntity.ok(response);
     }
