63777b58f8c58d0141a49e6b4c7156c5e598558a
juyeon <jjjjyy020704@naver.com>
2024-11-22 23:41:04 +0900

    8주차 미션 구현 완료

--- src/main/java/com/kuit/kuit4serverauth/KeyGenerator.java
+++ src/main/java/com/kuit/kuit4serverauth/KeyGenerator.java
@@ -4,16 +4,9 @@ import java.security.SecureRandom;
 import java.util.Base64;
 
 public class KeyGenerator {
-    /*public static void main(String[] args) {
-        SecureRandom random = new SecureRandom();
-        byte[] values = new byte[64]; // 512 bits
-        random.nextBytes(values);
-        String secureKey = Base64.getEncoder().encodeToString(values);
-        System.out.println("Secure Key: " + secureKey);
-    }*/
     public static String generateSafeToken() {
         SecureRandom random = new SecureRandom();
-        byte[] values = new byte[32]; // 256 bits are usually enough
+        byte[] values = new byte[32];
         random.nextBytes(values);
         return Base64.getEncoder().encodeToString(values);
     }
