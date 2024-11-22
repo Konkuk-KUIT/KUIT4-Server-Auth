00397fb59c621d125c38abe471b4091265acf288
jyun27 <kimjyun27@gmail.com>
2024-11-22 20:30:15 +0900

    feat: RefreshToken 발급 구현

--- src/main/java/com/kuit/kuit4serverauth/service/JwtUtil.java
+++ src/main/java/com/kuit/kuit4serverauth/service/JwtUtil.java
@@ -5,14 +5,19 @@ import com.kuit.kuit4serverauth.exception.ErrorCode;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String secret = "mysecretkey";
-    private final long expirationMs = 3600000; // 1 hour
+    @Value("${jwt.secret}")
+    private String secret;
+    @Value("${jwt.expiration}")
+    private String expirationMs;
+    @Value("${jwt.refreshExpiration}")
+    private String refreshExpirationMs;
 
     public String generateToken(String username, String role) {
         return Jwts.builder()
@@ -20,6 +25,15 @@ public class JwtUtil {
                 .claim("role", role)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public String gerenerateRefreshToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExpirationMs))
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
     }
@@ -32,6 +46,17 @@ public class JwtUtil {
                     .getBody();
         } catch (Exception e) {
             throw new CustomException(ErrorCode.INVALID_TOKEN);
+        }
+    }
+
+    public Claims validateRefreshToken(String token) {
+        try{
+            return Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        }catch (Exception e){
+            throw new CustomException(ErrorCode.INVALID_REFRESHTOKEN);
         }
     }
 }
