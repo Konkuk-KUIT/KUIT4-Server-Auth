196cfaa6f31ad74c495b344dc47090f65e5776e6
kisusu <kikisu115@naver.com>
2024-12-02 15:54:27 +0900

    delete: db 직접 사용을 위한 sql 파일 삭제

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (username, password, role) VALUES ('admin', 'admin123', 'ROLE_ADMIN');
-INSERT INTO users (username, password, role) VALUES ('user', 'user123', 'ROLE_USER');
--- src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS users;
-
-CREATE TABLE users (
-                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
-                       username VARCHAR(50) NOT NULL,
-                       password VARCHAR(100) NOT NULL,
-                       role VARCHAR(20) NOT NULL
-);
